f1a554704f2e5b5b088ac9ed57113699e8279a37
Gene <xinhaojing@Xinhaos-MacBook-Air.local>
2020-11-17 12:43:21 -0800

    update

--- LoginApp/LoginApp/LoginApp/SignUpViewController.swift
+++ LoginApp/LoginApp/LoginApp/SignUpViewController.swift
@@ -6,48 +6,54 @@
 //
 
 import UIKit
+import Firebase
+import SwiftSpinner
 
 class SignUpViewController: UIViewController {
     
     
     @IBOutlet weak var txtEmail: UITextField!
-    
-    
+        
     @IBOutlet weak var txtPassword: UITextField!
     
+    @IBOutlet weak var lblStatus: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func signUpAction(_ sender: Any) {
-        
         let email = txtEmail.text
         let password = txtPassword.text
         if email == "" || password!.count < 6 {
-            lblStatis.text = "Please enter email and correct password"
+            lblStatus.text = "Please enter email and correct password"
             return
         }
         if email?.isEmail == false {
-            lblStatis.text = "Please enter valid email"
+            lblStatus.text = "Please enter valid email"
             return
         }
         SwiftSpinner.show("Loggin in...")
-        Auth.auth().signIn(withEmail: email!, password: password!) { [weak self] authResult, error in
+//        Auth.auth().signIn(withEmail: email!, password: password!) { [weak self] authResult, error in
+//            SwiftSpinner.hide()
+//            guard let strongSelf = self else { return }
+//            if error != nil {
+//                strongSelf.lblStatus.text = error?.localizedDescription
+//                return
+//            }
+//            self?.txtPassword.text = ""
+//            self!.performSegue(withIdentifier: "loginSegue", sender: self)
+//        }
+        
+        Auth.auth().createUser(withEmail: email!, password: password!) { [weak self]authResult, error in
             SwiftSpinner.hide()
             guard let strongSelf = self else { return }
-            if error != nil {
-                strongSelf.lblStatis.text = error?.localizedDescription
+            guard let user = authResult?.user,error == nil else {
+                strongSelf.lblStatus.text = error?.localizedDescription
                 return
-                
             }
-            
-            self?.txtPassword.text = ""
-            
-            self!.performSegue(withIdentifier: "loginSegue", sender: self)
+            print("\(user.email!) created")
+            strongSelf.navigationController?.popViewController(animated: true)
         }
-        
-        
     }
 }
